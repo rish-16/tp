15fad3b7fc6559591fb96eb1a74c2aa5b6da38d4
didymus <51285113+didymental@users.noreply.github.com>
2021-10-22 23:43:45 +0800

    Make Entry not abstract (#117)

--- src/main/java/seedu/address/model/EntryList.java
+++ src/main/java/seedu/address/model/EntryList.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 import java.util.Comparator;
 
 /**
- * EntryList is an abstract class that uses only three main functions of a list - add, delete, sort
+ * EntryList is a class that uses only three main functions of a list - add, delete, sort
  */
-public abstract class EntryList<Entry> {
+public class EntryList<Entry> {
     private ArrayList<Entry> list = new ArrayList<>();
 
     public boolean add(Entry elem) {
